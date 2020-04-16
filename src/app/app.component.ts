473efc6a96f26206da35a631bb7407d812b0e441
Mary Denysova <djailin.vasu@gmail.com>
2020-04-16 21:52:23 +0300

    Определение состояний

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('box', [
+      state('start', style({
+        background: 'blue'
+      })),
+      state('end', style({
+        background: 'red',
+        transform: 'scale(1.2)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'angular-animations';
+  boxState = 'start';
+
+
+  animate() {
+    this.boxState = this.boxState === 'end' ? 'start' : 'end'
+  }
 }
