531020c7c8edd437ce92098ba244bc9436d95622
Mary Denysova <djailin.vasu@gmail.com>
2020-04-16 22:05:29 +0300

    Добавление transition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +13,9 @@ import { trigger, state, style } from '@angular/animations';
       state('end', style({
         background: 'red',
         transform: 'scale(1.2)'
-      }))
+      })),
+      transition('start => end', animate(300)),
+      transition('end => start', animate('800ms ease-in-out'))
     ])
   ]
 })
